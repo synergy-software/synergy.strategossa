3c5d954acac3ef334df0c911a9a22c32473de9b5
marcin.celej <marcin.celej@gmail.com>
2016-11-16 23:11:37 +0100

    Updated nuget packages

---
+++ Synergy.Strategossa.Web/App_Start/StrategossaWebLibrary.cs
@@ -0,0 +1,16 @@
+﻿using Synergy.Core;
+using Synergy.NHibernate;
+using Synergy.Web.Mvc;
+
+namespace Synergy.Strategossa.Web
+{
+    public class StrategossaWebLibrary : Library
+    {
+        public StrategossaWebLibrary() : base(
+            new SynergyWebMvcLibrary(),
+            new SynergyNHibernateLibrary(),
+            new SynergyCoreLibrary())
+        {
+        }
+    }
+}
